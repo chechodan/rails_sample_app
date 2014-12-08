e8563ffb3a37136b7ff77bee7c81b15b73b91a94
sedalo <sedalo.it@gmail.com>
2014-12-08 13:34:16 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,4 +8,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confiramation: "password" }
+    end
+    assert_template 'users/show'
+  end
 end

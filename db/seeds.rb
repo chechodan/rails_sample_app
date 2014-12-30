365132932dc01e7cc636e4e8d5919f91732f4e1e
sedalo <sedalo.it@gmail.com>
2014-12-30 17:07:36 -0300

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: "Example User", email: "example@ra.org", password: "example", password_confirmation: "example", admin: true)
+User.create!(name: "Example User", email: "example@ra.org", password: "example", password_confirmation: "example", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@ra.org"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
